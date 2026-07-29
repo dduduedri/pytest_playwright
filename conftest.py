132fddc4281c4e1616e0237e3bcc007e06b0b3b6
DAVIDED <David.Edry@Amdocs.com>
2026-07-29 15:58:51 +0300

    update

--- conftest.py
+++ conftest.py
@@ -29,8 +29,8 @@ from playwright.sync_api import Browser, Page, Playwright
 with open('data/execution_data.json') as _execution_data_file:
     execution_data = json.load(_execution_data_file)
 
-    
-def pytest_addoption(parser):
+
+def pytest_addoption(parser): #get the input from command line and if not exist use the execution_data.json
     parser.addoption(
         "--browser_name", action="store", default=execution_data["browser"], help="my option: chrome or firefox"
     )
@@ -43,7 +43,7 @@ def pytest_addoption(parser):
 def browser_setup(playwright: Playwright,request): # request gives access to global variables
     browser = playwright.chromium.launch(headless=False)
 
-    browser_name=request.config.getoption("--browser_name")  #get the arg from the command line : pytest -s test_e2e_framework_web_api_create_order.py --headed --browser_name firefox
+    browser_name=request.config.getoption("--browser_name")  #get the arg from the command line : pytest -s test_e2e_framework_web_api.py --headed --browser_name firefox
     if browser_name=="chrome":
         browser = playwright.chromium.launch(headless=False)
     elif browser_name=="firefox":
